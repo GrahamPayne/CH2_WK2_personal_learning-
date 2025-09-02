2304bd69ac294d261b769bcd04716a5154c9e02c
GrahamPayne <graham.woodlab@gmail.com>
2025-09-01 21:16:08 -0700

    Weak 2 chapter two personal learning

---
+++ Chapter2.py
@@ -0,0 +1,32 @@
+
+
+#Lesson 2:  Variables and Statements
+# Graham Unmacht-Payne, Intro to Python
+#part 1
+
+n = 17
+pi = 3.14159
+
+message = 'And now for something completely different.'
+
+#part 2
+
+print(n)
+print(pi)
+print (message)
+
+# part 3
+
+print(n + 25)
+print(2 * pi)
+print(round(pi))
+print(len(message))
+
+#part 4
+
+import math
+
+print(math.pi)
+print(math.sqrt(25))
+print(math.pow(5, 2))
+print(5 ** 2)
